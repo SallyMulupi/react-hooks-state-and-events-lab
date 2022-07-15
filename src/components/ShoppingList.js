45eafb3073fd1b81140860237a7ab4007fa50ee7
SallyMulupi <mulupisally2gmail.com>
2022-07-15 23:58:18 +0300

    add function handlecategory

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,3 +3,6 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
     const [selectCategory, setSelectCategory] = useState("All");
+    function handleCategory(event){
+      setSelectCategory(event.target.value);
+    }
