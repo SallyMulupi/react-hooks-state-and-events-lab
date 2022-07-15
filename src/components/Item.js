23db1868dc966529f98be79c1c85353e293b2b4c
SallyMulupi <mulupisally2gmail.com>
2022-07-16 00:06:55 +0300

    add  const incart and usestate

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 function Item({ name, category }) {
-  return (
-    <li className="">
-      <span>{name}</span>
-      <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
-    </li>
-  );
+    const [inCart, setInCart] = useState(false);
+    return ( <
+        li className = "" >
+        <
+        span > { name } < /span> <
+        span className = "category" > { category } < /span> <
+        button className = "add" > Add to Cart < /button> <
+        /li>
+    );
 }
 
 export default Item;
