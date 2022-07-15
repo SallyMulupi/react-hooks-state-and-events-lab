2102ba7bf556811a36fcdbabbf1785aab490ebdc
SallyMulupi <mulupisally2gmail.com>
2022-07-16 00:07:48 +0300

    add  const listclass

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,15 +1,16 @@
 import React from "react";
 
 function Item({ name, category }) {
-    const [inCart, setInCart] = useState(false);
-    return ( <
-        li className = "" >
-        <
-        span > { name } < /span> <
-        span className = "category" > { category } < /span> <
-        button className = "add" > Add to Cart < /button> <
-        /li>
-    );
+  const [inCart, setInCart] = useState(false);
+  const listClass = inCart ? "in-cart" : "";
+
+  return (
+    <li className="">
+      <span>{name}</span>
+      <span className="category">{category}</span>
+      <button className="add">Add to Cart</button>
+    </li>
+  );
 }
 
 export default Item;
